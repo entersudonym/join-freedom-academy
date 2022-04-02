0489f6cb3d33fc12ad6550e301010d6439caf488
entersudonym <noahtheheir@gmail.com>
2022-04-02 11:01:11 -0700

    kinda spaghet but pretty fire ux

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
 	import '../app.css';
+	import Fa from '../public/fa.png';
 </script>
 
-<header />
+<svelte:head>
+	<title>Freedom Academy</title>
+</svelte:head>
 
-<main>
-	<slot />
-</main>
+<div class="min-h-screen flex justify-center items-center bg-blue-50">
+	<div class="bg-slate-50 max-w-xl rounded-lg shadow-lg shadow-blue-500/20">
+		<img
+			src={Fa}
+			alt="A Freedom Academy-trained Spartan takes on the world"
+			class="h-40 w-full object-cover rounded-t-lg"
+		/>
+		<slot />
+	</div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,39 +1,24 @@
 <script context="module" lang="ts">
 	export const prerender = true;
-	import Fa from '../public/fa.png';
 </script>
 
-<svelte:head>
-	<title>Freedom Academy</title>
-</svelte:head>
+<div class="py-8 px-6 text-base space-y-4 text-gray-600">
+	<h1 class="text-2xl font-bold text-indigo-600">The Freedom Academy</h1>
+	<h4 class="text-base">
+		We are a focused self-discipline and self-mastery workshop for people who expect success and are
+		willing to work for it. We believe that:
+	</h4>
 
-<div class="min-h-screen flex justify-center items-center bg-blue-50">
-	<div class="bg-slate-50 max-w-xl rounded-lg shadow-lg shadow-blue-500/20">
-		<img
-			src={Fa}
-			alt="A Freedom Academy-trained Spartan takes on the world"
-			class="h-40 w-full object-cover rounded-t-lg"
-		/>
+	<ul class="space-y-2 list-inside list-disc">
+		<li>Discipline is freedom</li>
+		<li>Self-mastery is the key to all success in life</li>
+		<li>Helping others is the secret to happiness</li>
+	</ul>
 
-		<div class="py-8 px-6 text-base space-y-4 text-gray-600">
-			<h1 class="text-2xl font-bold text-indigo-600">The Freedom Academy</h1>
-			<h4 class="text-base">
-				We are a focused self-discipline and self-mastery workshop for people who expect success and
-				are willing to work for it. We believe that:
-			</h4>
-
-			<ul class="space-y-2 list-inside list-disc">
-				<li>Discipline is freedom</li>
-				<li>Self-mastery is the key to all success in life</li>
-				<li>Helping others is the secret to happiness</li>
-			</ul>
-
-			<a
-				class="px-6 py-2 bg-indigo-600 hover:bg-indigo-500 transition-colors rounded-lg text-white flex justify-center min-w-full"
-				href={'https://discordapp.com/invite/EqZgqEh'}
-			>
-				Join Us on Discord
-			</a>
-		</div>
-	</div>
+	<a
+		class="px-6 py-2 bg-indigo-600 hover:bg-indigo-500 transition-colors rounded-lg text-white flex justify-center min-w-full"
+		href={'https://discordapp.com/invite/EqZgqEh'}
+	>
+		Join Us on Discord
+	</a>
 </div>
