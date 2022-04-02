ff7175c92af9ae7ea51d50da9a935ea44fd511bb
entersudonym <noahtheheir@gmail.com>
2022-04-01 20:08:57 -0700

    Get the image to work

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script context="module" lang="ts">
 	export const prerender = true;
+	import Fa from '../public/fa.png';
 </script>
 
 <svelte:head>
@@ -8,13 +9,17 @@
 
 <div class="min-h-screen flex justify-center items-center bg-blue-50">
 	<div class="bg-slate-50 max-w-xl rounded-lg shadow-lg shadow-blue-500/20">
-		<div class="bg-[url('fa.png')] h-40 bg-center bg-cover rounded-t-lg"></div>
+		<img
+			src={Fa}
+			alt="A Freedom Academy-trained Spartan takes on the world"
+			class="h-40 w-full object-cover rounded-t-lg"
+		/>
 
 		<div class="py-8 px-6 text-base space-y-4 text-gray-600">
 			<h1 class="text-2xl font-bold text-indigo-600">The Freedom Academy</h1>
 			<h4 class="text-base">
-				We are a focused self-discipline and self-mastery workshop for people who
-				expect success and are willing to work for it. We believe that:
+				We are a focused self-discipline and self-mastery workshop for people who expect success and
+				are willing to work for it. We believe that:
 			</h4>
 
 			<ul class="space-y-2 list-inside list-disc">
@@ -25,7 +30,7 @@
 
 			<a
 				class="px-6 py-2 bg-indigo-600 hover:bg-indigo-500 transition-colors rounded-lg text-white flex justify-center min-w-full"
-				href={"https://discordapp.com/invite/EqZgqEh"}
+				href={'https://discordapp.com/invite/EqZgqEh'}
 			>
 				Join Us on Discord
 			</a>
